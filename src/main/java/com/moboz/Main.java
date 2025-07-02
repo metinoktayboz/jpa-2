b370be8f3cb802fdb959880f0937e1f08af884c7
moboz <metinoktayboz@gmail.com>
2025-07-02 14:33:55 +0300

    Initial commit

---
+++ src/main/java/com/moboz/Main.java
@@ -0,0 +1,39 @@
+package com.moboz;
+
+import com.moboz.entities.Employee;
+import com.moboz.persistence.CustomPersistenceUnitInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String puName = "pu-name";
+        Map<?, ?> properties = new HashMap<>();
+
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), properties);
+
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            Employee employee1 = em.find(Employee.class, 1);
+            employee1.setName("moBoz");
+
+//            em.persist();  -> Adding an entity in the context
+//            em.find();     -> Finds by PK, Get from DB and add it to the contest if doesnt already exist
+//            em.remove();   -> Marking Entity to removal
+//            em.merge();    -> Merges an Entity from outside the context to the context
+//            em.refresh();  -> Mirror the context from database
+//            em.detach();   -> Taking entity out of the context
+
+
+            em.getTransaction().commit();   //end of the transaction
+        }
+
+    }
+}
---
+++ src/main/java/com/moboz/persistence/CustomPersistenceUnitInfo.java
@@ -0,0 +1,121 @@
+package com.moboz.persistence;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+public class CustomPersistenceUnitInfo implements PersistenceUnitInfo {
+
+    private final String puName;
+
+    public CustomPersistenceUnitInfo(String puName) {
+        this.puName = puName;
+    }
+
+    @Override
+    public String getPersistenceUnitName() {
+        return puName;
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public String getScopeAnnotationName() {
+        return "";
+    }
+
+    @Override
+    public List<String> getQualifierAnnotationNames() {
+        return List.of();
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://localhost/demo");
+        dataSource.setUsername("root");
+        dataSource.setPassword("11223344");
+        return dataSource;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return List.of();
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return List.of();
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+        return List.of("com.moboz.entities.Employee");
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return "";
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
